6c779506e68bc85c42686837936c3acc7fdba8ba
dvniel <nexus@andromeda.local>
2024-12-19 16:36:02 +0100

    exos

--- qualite_du_code/clean_code_br1/exo-2.js
+++ qualite_du_code/clean_code_br1/exo-2.js
@@ -1,37 +1,25 @@
-const genUserReport = (nom, age, ville, profession, estAbonne) => {
+const genUserReport = (name, age, city, occupation, isSubscribed) => {
+  const status = isSubscribed ? 'Abonné' : 'Non abonné';
+  const category =
+    age < 18
+      ? `Jeune\n`
+      : age >= 18 && age < 60
+      ? `Adulte\n`
+      : `Senior\n`;
 
-  let rapport = `Rapport utilisateur :\n`;
-  rapport += `Nom : ${nom}\n`;
-  rapport += `Age : ${nom} ans\n`;
-  rapport += `Ville: ${ville}\n`;
-  rapport += `Profession : ${profession}\n`;
-
-  if (estAbonne == true) {
-    rapport += `Statut : Abonné\n`;
-  } else {
-    rapport += `Statut : Abonnée\n`;
-  }
-
-  if (age < 18) {
-    rapport += `Catégorie : Jeune\n`;
-  } else if (age >= 18 && age < 60) {
-    rapport += `Catégorie : jeune\n`;
-  } else {
-    rapport += `Catégorie : Senior\n`;
-  }
-
-  return rapport;
-
+  let report =
+    `
+    Rapport utilisateur :\n
+    Nom : ${name}\n
+    Age : ${age} ans\n
+    Ville ${city}\n
+    Profession : ${occupation}\n
+    Statut : ${status}\n
+    Catégorie : ${category}
+    `;
+  return report;
 }
-
-
-
-
-
-
-
-
-
+genUserReport('Daniel', '22', 'Bezons', 'Étudiant', true)
 
 function genererRapportUtilisateur(nom, age, ville, profession, estAbonne) {
     let rapport = "Rapport utilisateur :\n";
@@ -53,4 +41,3 @@ function genererRapportUtilisateur(nom, age, ville, profession, estAbonne) {
     }
     return rapport;
   }
-
